978f863e4151b170fcd43e45af8872b8a91f1194
Eric Liang <liang.eric24@gmail.com>
2016-11-12 09:18:11 -0800

    styles

--- AwesomeProject/index.ios.js
+++ AwesomeProject/index.ios.js
@@ -2,6 +2,51 @@ import {
   AppRegistry,
 } from 'react-native';
 
+
+//   StyleSheet,
+//   Text,
+//   View,
+//   TouchableHighlight,
+//   Navigator
+// } from 'react-native'
+// import UserSlider from './js/slider.js'
+// import Login from './js/login.js'
+
+// class AwesomeProject extends Component {
+//   _renderScene(route, navigator) {
+//     if (route.id == 0) {
+//       return (
+//         <View style={s.loginContainer}>
+//           <Text style={s.loginTitle}>Elephant Meets Donkey</Text>
+//           <Login navigator={navigator}/>
+//         </View>
+//       )
+//     } else if (route.id == 1) {
+//       return <UserSlider navigator={navigator}/>
+//     }
+//     // TODO: Create the id == 2 case
+//   }
+
 import App from './src/App';
 
 AppRegistry.registerComponent('AwesomeProject', () => App);
+
+// const s = StyleSheet.create({
+//   loginContainer: {
+//     flexDirection: 'column',
+//     flexWrap: 'wrap',
+//     alignItems: 'center',
+//     justifyContent: 'flex-end',
+//     paddingTop: 50,
+//   },
+//   loginTitle: {
+//     fontSize: 24,
+//     fontWeight: '700',
+//     marginBottom: 30,
+//   },
+//   loginButton: {
+//     marginTop: 100,
+//   }
+// })
+
+// AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject)
---
+++ AwesomeProject/js/login.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native'
+var {FBLogin, FBLoginManager} = require('react-native-facebook-login');
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { loggedIn: false, };
+  }
+
+  render() {
+    var _this = this;
+    return (
+      <FBLogin style={{ marginBottom: 10, }}
+        ref={(fbLogin) => { this.fbLogin = fbLogin }}
+        permissions={["email","user_friends"]}
+        loginBehavior={FBLoginManager.LoginBehaviors.Native}
+        onLogin={function(data){
+          console.log("Logged in!");
+          console.log(data);
+          _this.setState({
+            user : data.credentials,
+            loggedIn : true,
+          });
+          _this.props.navigator.push({id: 1})
+        }}
+        onLogout={function(){
+          console.log("Logged out.");
+          _this.setState({ user : null });
+        }}
+        onLoginFound={function(data){
+          console.log("Existing login found.");
+          console.log(data);
+          _this.setState({
+            user : data.credentials,
+            loggedIn : true,
+          });
+        }}
+        onLoginNotFound={function(){
+          console.log("No user logged in.");
+          _this.setState({ user : null });
+        }}
+        onError={function(data){
+          console.log("ERROR");
+          console.log(data);
+        }}
+        onCancel={function(){
+          console.log("User cancelled.");
+        }}
+        onPermissionsMissing={function(data){
+          console.log("Check permissions!");
+          console.log(data);
+        }}
+      />
+    );
+  }
+}
