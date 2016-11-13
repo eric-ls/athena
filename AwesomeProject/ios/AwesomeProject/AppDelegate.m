e70af6f3bdb4e9f40b126ebbb2b418302b4d7209
Eric Liang <liang.eric24@gmail.com>
2016-11-13 10:15:33 -0800

    intro page

--- AwesomeProject/ios/AwesomeProject/AppDelegate.m
+++ AwesomeProject/ios/AwesomeProject/AppDelegate.m
@@ -43,7 +43,7 @@
      * on the same Wi-Fi network.
      */
 
-    jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+    jsCodeLocation = [NSURL URLWithString:@"http://10.10.1.230:8081/index.ios.bundle?platform=ios&dev=true"];
 
     /**
      * OPTION 2
