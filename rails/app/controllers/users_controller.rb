4a74770045809ab62dc2b970e396caf24ecfa53e
Daniel Li <danielmxli@gmail.com>
2016-11-13 02:00:24 -0800

    Fix multiple user cereation bug

--- rails/app/controllers/users_controller.rb
+++ rails/app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 require 'json'
 class UsersController < ApplicationController
   def show
-    @user = User.find_or_create_by(user_params)
+    @user = User.create_with(user_params).find_or_create_by(facebook_id: user_params[:facebook_id])
     respond_to do |format|
       format.json { render json: @user }
     end
   end
 
   def create
-    @user = User.find_or_create_by(user_params)
+    # try and find with facebook_id, and create with user_params if not found
+    @user = User.create_with(user_params).find_or_create_by(facebook_id: user_params[:facebook_id])
     respond_to do |format|
       format.json { render json: @user }
     end
